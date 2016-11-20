7cfae989c1a8c531a54486a2088d51219a290372
yulelee <yulelee@stanford.edu>
2016-11-19 22:21:20 -0800

    utilities and contants

---
+++ utils.py
@@ -0,0 +1,9 @@
+
+################################################
+# constants for file handlers
+
+file_city_name = 'las_vegas'
+city_name = 'Las Vegas'
+
+# we only need the restaurants or users with more than this amount of reviews
+NUM_REVIEWS_THRESH = 20
