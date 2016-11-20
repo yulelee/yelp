1c3902917f30b556a097c219ce1f409c350eb9c0
yulelee <yulelee@stanford.edu>
2016-11-20 13:48:47 -0800

    added the training error, corrected the error in testing

--- base_line.py
+++ base_line.py
@@ -10,12 +10,19 @@ import utils
 
 train_data_matrix = load_matrix.train_data_matrix
 test_data_matrix = load_matrix.test_data_matrix
-all_predictions = np.zeros((load_matrix.n_users, load_matrix.n_items))
+test_predictions = np.zeros((load_matrix.n_users, load_matrix.n_items))
+train_predictions = np.zeros((load_matrix.n_users, load_matrix.n_items))
+
 
 for user_idx in range(load_matrix.n_users):
 	nonzero_index = np.nonzero(load_matrix.train_data_matrix[user_idx,])[0]
 	nonzero_values = load_matrix.train_data_matrix[user_idx, nonzero_index]
 	prediction = nonzero_values.mean()
-	all_predictions[user_idx, nonzero_index] = prediction
 
-print 'rmse:', utils.rmse(all_predictions, load_matrix.test_data_matrix)
+	train_predictions[user_idx, nonzero_index] = prediction
+
+	test_nonzero_index = np.nonzero(load_matrix.test_data_matrix[user_idx,])[0]
+	test_predictions[user_idx, test_nonzero_index] = prediction
+
+print 'training rmse:', utils.rmse(train_predictions, load_matrix.train_data_matrix)
+print 'testing rmse:', utils.rmse(test_predictions, load_matrix.test_data_matrix)
