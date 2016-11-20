5e2f5a330ab2bf2571df530ecefdf6d79cd356b1
yulelee <yulelee@stanford.edu>
2016-11-19 17:14:36 -0800

    store the index and id mappings for users and restaurants

---
+++ extract_matrix_index.py
@@ -0,0 +1,28 @@
+import utils
+import json
+
+file_city_name = utils.file_city_name
+
+# constructing the mappings from user_id and restaurant_id into matrix indices
+unique_user_counter, unique_rest_counter = 0, 0
+user_index_map, rest_index_map = {}, {}
+
+with open('data/' + file_city_name + '_reviews_filtered.json') as data_file:
+    for line in data_file:
+        review = json.loads(line)
+        if review['user_id'] not in user_index_map.keys(): 
+            user_index_map[review['user_id']] = unique_user_counter
+            unique_user_counter += 1
+            print user_index_map[review['user_id']]
+        if review['business_id'] not in rest_index_map.keys():
+            rest_index_map[review['business_id']] = unique_rest_counter
+            unique_rest_counter += 1
+
+print 'unique users:', unique_user_counter
+print 'unique restaurants:', unique_rest_counter
+
+# save the index mappings 
+with open('data/' + file_city_name + '_filtered_user_index.json', 'w') as user_idx_file:
+    json.dump(user_index_map, user_idx_file)
+with open('data/' + file_city_name + '_filtered_restaurants_index.json', 'w') as rest_idx_file:
+    json.dump(rest_index_map, rest_idx_file)
--- load_matrix.py
+++ load_matrix.py
@@ -1,8 +1,10 @@
 import numpy as np
 from sklearn import cross_validation as cv
 import pandas as pd
+import utils
+import json
 
-file_city_name = 'las_vegas'
+file_city_name = utils.file_city_name
 
 header = ['user_id', 'business_id', 'rating']
 df = pd.read_csv('data/' + file_city_name + '_reviews_ratings_only.txt', sep='\t', names=header)
@@ -12,15 +14,11 @@ n_items = df.business_id.unique().shape[0]
 
 print 'Number of users = ' + str(n_users) + ' | Number of items = ' + str(n_items)
 
-print 'create mappings for the user_id and item_id into matrix index'
-user_count, item_count = 0, 0
-user_id_map, item_id_map = {}, {}
-for user_id in df.user_id.unique():
-    user_id_map[user_id] = user_count
-    user_count += 1
-for item_id in df.business_id.unique():
-    item_id_map[item_id] = item_count
-    item_count += 1
+print 'load the index mappings'
+with open('data/' + file_city_name + '_filtered_user_index.json') as user_idx_file:
+    user_id_map = json.load(user_idx_file)
+with open('data/' + file_city_name + '_filtered_restaurants_index.json') as rest_idx_file:
+    item_id_map = json.load(rest_idx_file)
 
 print 'split and construct matrix'
 train_data, test_data = cv.train_test_split(df, test_size=0.25)
