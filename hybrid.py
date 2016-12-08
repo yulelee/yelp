eaab215c2a72a82e889d059867b512030890c3f2
yulelee <yulelee@stanford.edu>
2016-12-08 15:14:06 -0800

    also draw locally sorted curves

--- hybrid.py
+++ hybrid.py
@@ -16,6 +16,9 @@ file_city_name = utils.file_city_name
 train_data_matrix = load_matrix.train_data_matrix
 test_data_matrix = load_matrix.test_data_matrix
 
+plot_curve = False
+plot_step = True
+
 print 'Loading results from models...'
 
 # Those models' results are stored together in one matrix
@@ -65,25 +68,25 @@ print 'Constructing the features and labels...'
 # construct the features and labels for the linear regression
 hybrid_train_feature = np.column_stack((memory_based_cf_user_all[train_data_matrix.nonzero()], \
 										memory_based_cf_item_all[train_data_matrix.nonzero()], \
-										sgd_all[train_data_matrix.nonzero()], \
+										np.maximum(1, np.minimum(5, sgd_all[train_data_matrix.nonzero()])), \
 										# svd_all[train_data_matrix.nonzero()], \
 										tbr_preference_all[train_data_matrix.nonzero()], 
 										tbr_similarity_all[train_data_matrix.nonzero()]))
 
 hybrid_train_label = train_data_matrix[train_data_matrix.nonzero()]
 
-
-plt.plot(range(len(hybrid_train_label)), np.sort(hybrid_train_label), label='Labels');
-plt.plot(range(len(hybrid_train_label)), np.sort(hybrid_train_feature[:,0]), label='Feature1');
-plt.plot(range(len(hybrid_train_label)), np.sort(hybrid_train_feature[:,1]), label='Feature2');
-plt.plot(range(len(hybrid_train_label)), np.sort(hybrid_train_feature[:,2]), label='Feature3');
-plt.plot(range(len(hybrid_train_label)), np.sort(hybrid_train_feature[:,3]), label='Feature4');
-plt.plot(range(len(hybrid_train_label)), np.sort(hybrid_train_feature[:,4]), label='Feature5');
+if plot_curve:
+	plt.plot(range(len(hybrid_train_label)), np.sort(hybrid_train_label), label='Labels');
+	plt.plot(range(len(hybrid_train_label)), np.sort(hybrid_train_feature[:,0]), label='Feature1');
+	plt.plot(range(len(hybrid_train_label)), np.sort(hybrid_train_feature[:,1]), label='Feature2');
+	plt.plot(range(len(hybrid_train_label)), np.sort(hybrid_train_feature[:,2]), label='Feature3');
+	plt.plot(range(len(hybrid_train_label)), np.sort(hybrid_train_feature[:,3]), label='Feature4');
+	plt.plot(range(len(hybrid_train_label)), np.sort(hybrid_train_feature[:,4]), label='Feature5');
 
 
 hybrid_test_feature = np.column_stack((memory_based_cf_user_all[test_data_matrix.nonzero()], \
 										memory_based_cf_item_all[test_data_matrix.nonzero()], \
-										sgd_all[test_data_matrix.nonzero()], \
+										np.maximum(1, np.minimum(5, sgd_all[test_data_matrix.nonzero()])), \
 										# svd_all[test_data_matrix.nonzero()], \
 										tbr_preference_all[test_data_matrix.nonzero()], 
 										tbr_similarity_all[test_data_matrix.nonzero()]))
@@ -102,17 +105,40 @@ hybrid_test_feature = poly.fit_transform(hybrid_test_feature)
 print hybrid_train_feature.shape
 
 # regr = linear_model.LinearRegression()
-regr = linear_model.Ridge()
+regr = linear_model.LinearRegression()
 regr.fit(hybrid_train_feature, hybrid_train_label)
 
 train_prediction = regr.predict(hybrid_train_feature)
 test_prediction = regr.predict(hybrid_test_feature)
 
-plt.plot(range(len(hybrid_train_label)), np.sort(train_prediction), label='predictions');
+if plot_curve:
+	plt.plot(range(len(hybrid_train_label)), np.sort(train_prediction), label='predictions');
+	plt.legend()
+	plt.grid()
+	plt.show()
 
-plt.legend()
-plt.grid()
-plt.show()
+def plot_step_helper(preds, labels):
+	label_with_index = [(label, index) for (index, label) in enumerate(labels)]
+	label_with_index.sort()
+	reverse_index = [old_index for (_, old_index) in label_with_index]
+
+	one = np.sort(preds[np.where(labels == 1)])
+	two = np.sort(preds[np.where(labels == 2)])
+	three = np.sort(preds[np.where(labels == 3)])
+	four = np.sort(preds[np.where(labels == 4)])
+	five = np.sort(preds[np.where(labels == 5)])
+	sorted_predctions = np.concatenate((one, two, three, four, five))
+
+
+	plt.plot(range(len(labels)), sorted_predctions, label='predictions');
+	plt.plot(range(len(labels)), labels[reverse_index], label='label')
+	plt.legend()
+	plt.grid()
+	plt.show()
+
+if plot_step:
+	plot_step_helper(train_prediction, hybrid_train_label)
+	plot_step_helper(test_prediction, hybrid_test_label)
 
 print 'training rmse:', utils.list_rmse(train_prediction, hybrid_train_label)
 print 'testing rmse:', utils.list_rmse(test_prediction, hybrid_test_label)
